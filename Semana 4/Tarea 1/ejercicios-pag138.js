88d7d456e625a1c96439421ebc7219fa9f008c94
gonzalocarhuachin <lgonzalocarhuachin@gmail.com>
2020-04-30 14:35:56 -0500

    ejercicios

---
+++ Semana 4/Tarea 1/ejercicios-pag138.js
@@ -0,0 +1,35 @@
+//4.1
+
+let salarioInicial = 1500;
+let incrementoSalario = salarioInicial * 1.16;
+let salarioActual = 0;
+let i = 0;
+
+while(i<6)
+{
+    salarioActual = incrementoSalario++;
+    i++;
+    console.log(`Salario actual despues de 6 años: ${salarioActual}`);
+}
+
+//4.3
+
+let numeros = [2, 3, 4, 0, 4, 23, 0, 0, 11, -2, -4, -10, -1, 50];
+let numerosCero = [];
+let numerosMayoresCero = [];
+let numerosMenoresCero = [];
+
+if(numeros === 0)
+{
+
+}
+
+//4.9
+
+let deposito = 200;
+let inversionFinal = 0;
+let c = 0;
+
+
+
+//4.13
