e6d58615d322889289287231a534d1cfb9b373a3
gonzalocarhuachin <lgonzalocarhuachin@gmail.com>
2020-05-01 14:30:40 -0500

    p168

---
+++ Semana 4/Tarea 2/ejercicios-pag168.js
@@ -0,0 +1,18 @@
+//5.6
+
+let alumnos = [];
+for(let i = 0; i < 10; i++)
+{
+    let alumno = [];
+    alumno[0] = prompt("Ingrese nombre:");
+    alumno[1] = +prompt("Ingrese edad:");
+    alumnos.push(alumno);
+}
+
+for(let c = 0;c < alumnos.length; c++)
+{
+    if(alumnos[c[1]])
+    {
+
+    }
+}
